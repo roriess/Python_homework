385bc87b4dd22476726a5096d31b727ea52a70e3
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-13 14:51:06 +0300

    added decode for text and dictionary creation has been moved to a separate function

--- src/Homework_8/for_text.py
+++ src/Homework_8/for_text.py
@@ -1,7 +1,7 @@
 from binary_tree_structure import Tree
 from huffman import huffman
 
-def encode(string):
+def dictionary(string):
     freq = {}
     for elm in string:
         freq[elm] = freq[elm] + 1 if elm in freq else 1
@@ -19,17 +19,37 @@ def encode(string):
         nodes.sort(key=lambda x: x[1])
 
     root = nodes[0][0]
-    huffman_code = huffman(root)
+    
+    return huffman(root)
 
+
+def encode(string):
     encoded_message = ''
+    huffman_code = dictionary(string)
 
     for elm in string:
         encoded_message += huffman_code[elm]
 
     return encoded_message
 
-def decode(encode_message):
-    pass
+
+def decode(string, encode_message):
+    huffman_dict = dictionary(string)
+    reversed_huffman_dict = {v: k for k, v in huffman_dict.items()} 
+    decode_message = ''
+    print(huffman_dict.items())
+
+    current_elm = ''
+    for i in range(0, len(encode_message)):
+        current_elm += encode_message[i]
+        if current_elm in huffman_dict.values():
+            decode_message += reversed_huffman_dict.get(current_elm)
+            current_elm = ''
+
+    return decode_message
+
+
+
 
 
 string = 'there should be text that needs to be encoded'
@@ -37,5 +57,5 @@ string = 'there should be text that needs to be encoded'
 encode_message = encode(string)
 print(f'encoded message: {encode_message}')
 
-decode_message = decode(encode_message)
+decode_message = decode(string, encode_message)
 print(f'decode message: {decode_message}')
