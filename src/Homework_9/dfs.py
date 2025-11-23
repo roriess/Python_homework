5137ed497e3a7daea2842223201a44a07f584e74
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-23 21:34:33 +0300

    added dfs algorithm with an uniterable graph

---
+++ src/Homework_9/dfs.py
@@ -0,0 +1,44 @@
+class Graph:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, node):
+        if node not in self.nodes: 
+            self.nodes[node] = []
+
+    def add_edge(self, node1, node2):
+        if node1 in self.nodes and node2 in self.nodes:
+            self.nodes[node1].append(node2)
+
+    def get_neighbors(self, node):
+        return self.nodes.get(node, [])
+    
+
+def dfs(graph: Graph) -> list:
+    visited = []
+    stack = []
+
+    start_node = list(graph.nodes.keys())[0]
+    stack.insert(0, start_node)
+
+    while stack:
+        node = stack.pop(0)
+        if node not in visited:
+            visited.append(node)
+            
+        for neighbor in graph.get_neighbors(node):
+            if neighbor not in visited and neighbor not in stack:
+                stack.insert(0, neighbor)
+
+    return visited  
+
+
+graph = Graph()
+graph.add_node("A")
+graph.add_node("B")
+graph.add_node("C")
+graph.add_edge("A", "C")
+graph.add_edge("C", "B")
+
+
+print(dfs(graph))
