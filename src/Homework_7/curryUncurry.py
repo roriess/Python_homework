1b060b633c96ef9cb6b5a7b60fdf32aebdec7f17
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-12-08 17:13:47 +0300

    curry has been changed

--- src/Homework_7/curryUncurry.py
+++ src/Homework_7/curryUncurry.py
@@ -1,28 +1,9 @@
-def checkArgsCount(countOfArgs):
-    if countOfArgs > 3 or countOfArgs < 0: return 0
-    else: return 1
-
-def sum3(x, y, z):
-    return x + y + z
-
-
-def curry(sum3, countElm):
-    return lambda a: lambda b: lambda c: sum3(a, b, c)
-
-
-def uncurry(sum3Curry, countElm):
-    return lambda a, b, c: sum3Curry(a)(b)(c)
-
-
-countOfArgs = int(input("Enter a count of args: "))
-
-if (checkArgsCount(countOfArgs) == 1):
-    argA, argB, argC = map(int, input("Enter args: ").split())
-    
-    sum3Curry = curry(sum3, countOfArgs)
-    sum3Uncurry = uncurry(sum3Curry, countOfArgs)
-    
-    print(f'After curry: {sum3Curry(argA)(argB)(argC)}')
-    print(f'After uncurry: {sum3Uncurry(argA, argB, argC)}')
-
-else: print("Error: count of args is incorrect")
+def curry(func, n):
+    if int(n) == n and n > 0 and func.__code__.co_argcount == n:
+        def curried(*args):
+            if len(args) >= func.__code__.co_argcount:
+                return func(*args)
+            return lambda *args2: curried(*(args + args2)) # если было передано недостаточно аргументов
+        return curried
+    else: 
+        raise ValueError("Incorrect n was transmitted")
